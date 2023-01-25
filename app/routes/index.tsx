96f2f7e14951ab2267e7d13425164b4ce22f6e84
Martin-Luther Obahor <kingm4159@gmail.com>
2023-01-25 01:21:04 +0100

    Delete app directory

--- app/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Index() {
-  return (
-      <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.4", display: 'flex', alignItems: 'center', flexDirection:'column'}}>
-        <h1>Cateyes</h1>
-        <h2 style={{justifySelf: 'center'}}>Under Construction</h2>
-
-      </div>
-  );
-}
