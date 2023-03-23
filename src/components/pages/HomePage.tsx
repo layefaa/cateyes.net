13c7c1db7ebd12b48f4dc7245ae0f506347a9f14
Martin-Luther Obahor <kingm4159@gmail.com>
2023-03-24 00:07:22 +0100

    perf:

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import {Hero, ShortAbout, ShortWorks} from "@/components/templates";
+import React, {lazy} from 'react';
+import {Hero} from "@/components/templates";
+
+const ShortAbout = lazy(() => import('@/components/templates/ShortAbout'));
+const ShortWorks = lazy(() => import('@/components/templates/ShortWorks'));
 
 const HomePage = () => {
   return (
@@ -14,7 +17,6 @@ const HomePage = () => {
           <ShortWorks/>
         </section>
         <section/>
-
 
 
       </>
