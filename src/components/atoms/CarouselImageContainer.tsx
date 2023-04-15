0966affacf55632f874955a7e68fc04db73e7886
Martin-Luther Obahor <kingm4159@gmail.com>
2023-04-15 06:11:36 +0100

    update on images

--- src/components/atoms/CarouselImageContainer.tsx
+++ src/components/atoms/CarouselImageContainer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CarouselImageContainer = ({img}: { img: string }) => {
   return (
       // eslint-disable-next-line @next/next/no-img-element
-        <img src={img} alt='' loading={'lazy'} className={'object-cover object-top h-full w-full'}/>
+        <img src={img} alt='' loading='lazy' className={'object-cover object-top h-full w-full'}/>
   );
 };
 
