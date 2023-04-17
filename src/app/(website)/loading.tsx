7e9bd53e340f81cb36e376446305138e10a7b727
Martin-Luther Obahor <kingm4159@gmail.com>
2023-04-17 15:34:39 +0100

    testing speed

--- src/app/(website)/loading.tsx
+++ src/app/(website)/loading.tsx
@@ -1,7 +1,7 @@
 
 export default function Loading() {
   return (
-      <div className={'bg-red-200 h-screen w-full'}>
+      <div className={'h-screen w-full'}>
        Loading
       </div>
   )
--- src/components/atoms/ProfileImage.tsx
+++ src/components/atoms/ProfileImage.tsx
@@ -4,8 +4,8 @@ import Image from "next/image"
 const ProfileImage = ({url}: { url: string }) => {
   return (
       <div className={'relative w-full aspect-[0.75] md:aspect-[0.94] md:h-full '}>
-        <Image priority src={url} alt={''} fill
-               sizes="100vw"
+        <Image src={url} alt={''} fill
+               sizes="100%"
                style={{
                  objectPosition: 'top center',
                  objectFit: 'cover',
