9090a5b6f6c4a262fce3ad73fbb26a863e013101
Martin-Luther Obahor <kingm4159@gmail.com>
2023-11-03 03:57:01 +0100

    clean up

--- src/components/molecules/Carousel.tsx
+++ src/components/molecules/Carousel.tsx
@@ -8,32 +8,32 @@ import {CarouselPagination} from "@/utils/CarouselPagination";
 
 
 const Carousel = ({images}: { images: Array<object> }) => {
-      // const shouldSwiperUpdate = useCallback(() => false, []);
-      SwiperCore.use([Pagination, Autoplay])
-      // @ts-ignore
-      return (
-          <Swiper
-              autoplay
-              loop
-              slidesPerView={1}
-              pagination={CarouselPagination}
-              modules={[Pagination]}
-          >
-            {
-              images.map((img: any) => {
-                    return (
-                        <SwiperSlide key={img.url}>
-                          <div className={'relative aspect-[0.65] md:aspect-[1.5]  lg:aspect-[2.0] 2xl:aspect-[2.5]'}>
-                            <CarouselSlideContainer img={img}/>
-                          </div>
-                        </SwiperSlide>
-                    )
-                  }
-              )
-            }
-          </Swiper>
-      );
-    }
+     // const shouldSwiperUpdate = useCallback(() => false, []);
+     SwiperCore.use([Pagination, Autoplay])
+     // @ts-ignore
+     return (
+        <Swiper
+           autoplay
+           loop
+           slidesPerView={1}
+           pagination={CarouselPagination}
+           modules={[Pagination]}
+        >
+          {
+            images.map((img: any) => {
+                 return (
+                    <SwiperSlide key={img.url}>
+                      <div className={'relative aspect-[0.65] md:aspect-[1.5]  lg:aspect-[2.0] 2xl:aspect-[2.5]'}>
+                        <CarouselSlideContainer img={img}/>
+                      </div>
+                    </SwiperSlide>
+                 )
+               }
+            )
+          }
+        </Swiper>
+     );
+   }
 ;
 
 export default Carousel;
--- src/components/organisms/FollowEffect.tsx
+++ src/components/organisms/FollowEffect.tsx
@@ -4,7 +4,7 @@ import {Socials} from "@/components/molecules";
 const FollowEffect = () => {
   return (
       <div className={`group w-fit`}>
-        <div className={`flex items-center gap-[1.6rem] cursor-pointer hidden sm:flex group-hover:hidden w-fit`}>
+        <div className={` items-center gap-[1.6rem] cursor-pointer hidden sm:flex group-hover:hidden w-fit`}>
           <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1 7H13" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
             <path d="M7 1V13" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
